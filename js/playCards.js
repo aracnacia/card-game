071942209b88cd5e07216010884c5559aec3d163
aracnacia <wongdarren777@gmail.com>
2015-03-03 15:43:11 -0800

    Update playCards.js

--- js/playCards.js
+++ js/playCards.js
@@ -16,7 +16,7 @@ $(document).ready(function(){
         for(var i=0;i<hand.length;i++){
             el.append(hand[i].getHTML());
         }
-           el = $('#computerHand')
+           el = $('#ComputerHand')
         el.html('');
         for(var i=0;i<hand1.length;i++){
             el.append(hand1[i].getHTML());
